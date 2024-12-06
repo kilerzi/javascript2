a1461c8708940bf1913ac196342f1368fc13894c
kilerzi <mihailsavovski123123@gmail.com>
2024-12-06 22:53:10 +0100

    exercise1

---
+++ exercise1/java.js
@@ -0,0 +1,16 @@
+let input=prompt("Enter how much money you have");
+let aaa=null;
+if(input === aaa)
+{
+
+          if(input>=10){ 
+            console.log("u should go to a nice restoruant");
+          }
+          else
+         {
+            console.log("u should save up")
+           }
+        
+    }else{
+        console.log(aaa)
+    }
